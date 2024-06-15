bb1f8c320c7cf62452365fce21b19d13473569eb
Valentin Repetto <valentinrepetto12345@gmail.com>
2024-06-15 19:40:53 -0300

    update next auth middleware

--- middleware.ts
+++ middleware.ts
@@ -1,3 +1,3 @@
 export { default } from "next-auth/middleware";
 
-export const config = { matcher: ["/dashboard", "/dashboard/accounts"] };
+export const config = { matcher: ["/dashboard", "/dashboard/products"] };
